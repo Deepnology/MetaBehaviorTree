e0d806d6aea93ad9bb18b7482baad55e685002d6
deepnology <deepnology@icloud.com>
2019-08-03 11:25:31 -0400

    remove header

--- btree.hpp
+++ btree.hpp
@@ -1,8 +1,3 @@
-/*****************************************
-	The Meta Behavior Tree Library
-	Copyright (c) 2017 David Feng
-	Distributed under the MIT License.
-*****************************************/
 #ifndef BTREE_HPP
 #define BTREE_HPP
 namespace mbt
